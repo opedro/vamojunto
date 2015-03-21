952d11048d8f2d79dac264891349d46a19eb031a
Pedro Ruiz <opedro.ruiz@gmail.com>
2015-03-20 21:34:13 -0300

    Inicio menu suspenso

---
+++ scripts/default.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('#user-detail').on('click', function(){
+		$menu = $('#side-menu');
+		if($menu.hasClass('show'))
+			$menu.slideUp();
+		else
+			$menu.slideDown();
+		$menu.toggleClass('show')
+	})
+})
