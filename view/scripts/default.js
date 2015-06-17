8cfd028162d51bc9f14c418174c1243d52da3dd4
Pedro Ruiz <opedro.ruiz@gmail.com>
2015-06-16 23:47:13 -0300

    commit by VSCode

--- view/scripts/default.js
+++ view/scripts/default.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	$('#user-detail').on('click', function(){
 		$menu = $('#side-menu');
-		if($menu.hasClass('show'))
+		if($menu.hasClass('show')){
 			$menu.slideUp();
-		else
+		}else{
 			$menu.slideDown();
+		}
 		$menu.toggleClass('show')
 	})
 })
